99c1d1fed9b9d816770927d8ebaed13be42eeed8
Loc Nguyen <kten1112@gmail.com>
2021-05-04 12:38:23 +0700

    #61 Select entry method to request surfacewater license

--- resources/views/page/quan-ly-cap-phep/tnn-tao-moi-giay-phep-nuoc-mat.blade.php
+++ resources/views/page/quan-ly-cap-phep/tnn-tao-moi-giay-phep-nuoc-mat.blade.php
@@ -40,12 +40,33 @@
                 <span class="col-md-4 font-13 px-0">Chọn cách nhập</span>
                 <select name="entry-method" id="entry-method" class="col-5 font-13 py-1 px-2">
                     <option value="">Chọn cách nhập dữ liệu</option>
-                    <option value="">Nhập file excel</option>
-                    <option value="">Nhập thủ công</option>
+                    <option value="form-upload-surfacewater-license">Nhập file excel</option>
+                    <option value="form-create-surfacewater-license">Nhập trường dữ liệu</option>
                 </select>
             </div>
         </div>
-        <form action="{{route('xu-ly-tao-moi-giay-phep-nuoc-mat')}}" method="POST" enctype="multipart/form-data">
+        <div class="exploit-surfacewater col-12 mb-1 my-1" id="form-upload-surfacewater-license">
+            <div class="col-md-12 col-12 d-flex pl-0 pr-0 pr-md-3 mb-1 my-md-2 align-items-center justify-content-end justify-content-md-start">
+                <span class="col-md-4 font-13 px-0">Loại công trình</span>
+                <select name="entry-method" id="entry-method" class="col-5 font-13 py-1 px-2 mr-2">
+                    <option value="">Chọn loại công trình</option>
+                    <option value="1">Thủy điện</option>
+                    <option value="2">Thủy lợi</option>
+                    <option value="3">Công trình khác</option>
+                </select>
+                <button class="btn btn-info font-13">File mẫu</button>
+            </div>
+            <span><i>Người dùng chọn loại công trình để lấy file mẫu, điền dữ liệu theo đúng định dạng và tải lên ở dưới đây</i></span>
+            <div class="col-md-12 col-12 d-flex pl-0 pr-0 pr-md-3 mb-1 my-md-2 align-items-center justify-content-end justify-content-md-start">
+                <span class="col-md-4 font-13 px-0">File excel (.xls, .xlsx)</span>
+                <input type="file" class="col-6 font-13 py-1 px-2 mr-2" name="file-upload-surfacewater">
+            </div>
+            <div class="col-md-12 col-12 d-flex pl-0 pr-0 pr-md-3 mb-1 my-md-2 align-items-center justify-content-end justify-content-md-start">
+                <span class="col-md-4 font-13 px-0"></span>
+                <input type="submit" class="col-3 font-13 py-1 btn btn-success" name="submit-file-upload-surfacewater" value="Xác nhận">
+            </div>
+        </div>
+        <form action="{{route('xu-ly-tao-moi-giay-phep-nuoc-mat')}}" method="POST" enctype="multipart/form-data" id="form-create-surfacewater-license">
         {{ csrf_field() }}
             <div class="exploit-surfacewater mb-2">
                 <p class="col-12 py-1 exploit-surfacewater-title font-weight-bold mb-2">Tên tổ chức cá nhân đề nghị cấp phép</p>
@@ -436,6 +457,24 @@
         $("#btn_submit").click(function(){
             localStorage.removeItem("ct_dau_moi");
         })
+
+        $("#entry-method").on('change',function(){
+            if($(this).val() == 'form-upload-surfacewater-license')
+            {
+                $("#form-upload-surfacewater-license").show();
+                $("#form-create-surfacewater-license").hide();
+            }
+            else if($(this).val() == 'form-create-surfacewater-license')
+            {
+                $("#form-upload-surfacewater-license").hide();
+                $("#form-create-surfacewater-license").show();
+            }
+            else
+            {
+                $("#form-upload-surfacewater-license").hide();
+                $("#form-create-surfacewater-license").hide();
+            }
+        });
     </script>
 </main>
 @endsection
